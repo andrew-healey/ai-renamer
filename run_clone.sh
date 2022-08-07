7e2771f556912fe86173e602d104827ad98a6cd6
Andrew Healey <doolie.healey@gmail.com>
2022-08-07 08:47:05 -0400

    Remove ssh-add

--- run_clone.sh
+++ run_clone.sh
@@ -3,6 +3,5 @@ echo $SUBREPO_KEY > /app/id_rsa_refactor
 ls -a /
 
 echo $(cat key_info.txt) > /app/ssh_config
-ssh-add /app/ssh_config
 
 GIT_SSH_COMMAND='ssh -i /app/ssh_config -o IdentitiesOnly=yes' git clone git@github.com:andrew-healey/shift-refactor.git
