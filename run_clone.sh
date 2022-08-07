c498683df4acfe998df41aeca5465a5bcd3b0634
Andrew Healey <doolie.healey@gmail.com>
2022-08-07 08:33:45 -0400

    Testing etc ssh

--- run_clone.sh
+++ run_clone.sh
@@ -2,6 +2,6 @@ echo $SUBREPO_KEY > /app/id_rsa_refactor
 
 ls /
 
-echo $(cat key_info.txt) > ~/.ssh/config
+echo $(cat key_info.txt) > /etc/ssh/ssh_config
 
 git clone git@github-refactor:andrew-healey/shift-refactor.git
