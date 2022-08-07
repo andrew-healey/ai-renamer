8a7df166e69357f6c3f8b9f0143eefb91400d418
Andrew Healey <doolie.healey@gmail.com>
2022-08-07 09:02:14 -0400

    Try and include dev deps?

--- run_clone.sh
+++ run_clone.sh
@@ -1,9 +1,3 @@
 echo $SUBREPO_KEY | tr " " "\n" | tr "," " " > /app/id_rsa_refactor
 
-cat /app/id_rsa_refactor
-
-ls -a /
-
-echo $(cat key_info.txt) > /app/ssh_config
-
 GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no -i /app/id_rsa_refactor -o IdentitiesOnly=yes' git clone git@github.com:andrew-healey/shift-refactor.git
