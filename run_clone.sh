a8d641d5e8323fa32d885de91039a3e5e70dedb1
Andrew Healey <doolie.healey@gmail.com>
2022-08-07 08:45:57 -0400

    One-liner

--- run_clone.sh
+++ run_clone.sh
@@ -5,4 +5,4 @@ ls -a /
 echo $(cat key_info.txt) > /app/ssh_config
 ssh-add /app/ssh_config
 
-git clone git@github.com-refactor:andrew-healey/shift-refactor.git
+GIT_SSH_COMMAND='ssh -i /app/ssh_config -o IdentitiesOnly=yes' git clone git@github.com:andrew-healey/shift-refactor.git
